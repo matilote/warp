e3f497d60c34a7cc9656445d8b01094806c83fb9
Mateusz Jędrzejewski <33068017+matilote@users.noreply.github.com>
2022-01-27 01:54:06 +0100

    Update copy_binaries.sh

--- scripts/copy_binaries.sh
+++ scripts/copy_binaries.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # TEMP - Moves isoltest, kudu arm64 binaries to destination directory
-cp /home/ubuntu/warp/src/warp/bin/linux/isoltest /tmp/warp/src/warp/bin/linux/isoltest
-cp /home/ubuntu/warp/src/warp/bin/linux/kudu /tmp/warp/src/warp/bin/linux/kudu
+cp /home/ubuntu/warp/src/warp/bin/linux/isoltest /home/ubuntu/warp-temporary/src/warp/bin/linux/isoltest
+cp /home/ubuntu/warp/src/warp/bin/linux/kudu /home/ubuntu/warp-temporary/src/warp/bin/linux/kudu
