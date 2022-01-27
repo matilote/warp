ce79d438052a8503bec32dcb9dfb0250536e9f83
Mateusz Jędrzejewski <33068017+matilote@users.noreply.github.com>
2022-01-27 02:37:27 +0100

    Update run_test_semantics.sh

--- scripts/run_test_semantics.sh
+++ scripts/run_test_semantics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source $HOME/.cargo/env
 cd /home/ubuntu/warp-temporary
 python -m venv venv
 source venv/bin/activate
+source $HOME/.cargo/env
 pip install poetry
 poetry run make test_semantics
